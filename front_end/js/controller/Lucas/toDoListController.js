cd41d6034e50c06deca317954acd39a9dfc717db
Lucas Caixeta <contato@lucascaixeta.com>
2015-03-01 18:23:59 -0300

    Lucas's ToDo List and some improvements
    I rearranged the directories and files and put my part , starting my To
    Do List.

---
+++ front_end/js/controller/Lucas/toDoListController.js
@@ -0,0 +1,6 @@
+function TodoCtrl($scope){
+  $scope.todos = [
+    {done: false, text: 'first'},
+    {done: false, text: 'second'}
+  ];
+}
--- front_end/js/router/Router.js
+++ front_end/js/router/Router.js
@@ -1,21 +1,26 @@
 angular.module('AngularPoc', ['ngRoute']).config(function ($routeProvider) {
     'use strict';
 
-	/*Mauricio routes*/
-	$routeProvider.when('/mauricio', {
+    /*Mauricio routes*/
+    $routeProvider.when('/mauricio', {
         controller : MauricioController,
         templateUrl : 'templates/mauricio/mauricio.html'
     });
-	
-	$routeProvider.when('/mauricio/cadastrarFrutas', {
+
+    $routeProvider.when('/mauricio/cadastrarFrutas', {
         controller : MauricioController,
         templateUrl : 'templates/mauricio/cadastrarFrutas.html'
-	});
-	
-	/*Gabriel routes*/
+    });
+
+    /*Gabriel routes*/
     $routeProvider.when('/gabriel', {
         controller : GabrielController,
-        templateUrl : 'templates/gabrielController.html'
+        templateUrl : 'templates/gabriel/gabrielController.html'
+    });
 
+    /*Lucas routes*/
+    $routeProvider.when('/lucas', {
+        controller : TodoCtrl,
+        templateUrl : 'templates/lucas/toDoListTemplate.html'
     });
 });
