5fa746d7686dd9a66eec39950359c8527f3dfca1
Lucas Caixeta <contato@lucascaixeta.com>
2015-03-01 19:32:25 -0300

    Now I can add new Itens on my list \o_ /o/
    First, I implemented a form with a ng-submit directive. Its call
    addTodo() function on my controller. When addTodo is called its create a
    new item on my to do list.
    I put some comments on my files too and I followed JSDoc pattern to do
    it.

--- front_end/js/controller/Lucas/toDoListController.js
+++ front_end/js/controller/Lucas/toDoListController.js
@@ -1,6 +1,36 @@
-function TodoCtrl($scope){
-  $scope.todos = [
-    {done: false, text: 'first'},
-    {done: false, text: 'second'}
-  ];
+/**
+ * This is my To Do List Controller, where I´ll put
+ * all my controller functions. 
+ *
+ * Maurício, Brayan, Julio, Gabriel and everyone, look at http://en.wikipedia.org/wiki/JSDoc. 
+ * Is better for our project (this :-]) use JSDoc pattern to document all stuff.
+ * 01/03/2015 - 19:12
+ * 
+ * @author Lucas Caixeta
+ * @param {$scope} scope is an object that refers to the application model.
+ */
+function TodoCtrl($scope) {
+    'use strict';
+    
+    /*
+    * Create my first two 'To Do´s'.
+    * Done: for checkbox, if true, checkbox will be checked.
+    * text: my todo description.
+    */
+    $scope.todos = [
+        {done: false, text: 'first'},
+        {done: false, text: 'second'}
+    ];
+
+    /*
+    * Add a new ToDo after submit form, very simple and fun :)
+    */
+    $scope.addTodo = function () {
+        var newTodo = {
+            done: false,
+            text: $scope.todoText
+        };
+        
+        $scope.todos.push(newTodo);
+    };
 }
--- front_end/js/router/Router.js
+++ front_end/js/router/Router.js
@@ -18,7 +18,9 @@ angular.module('AngularPoc', ['ngRoute']).config(function ($routeProvider) {
         templateUrl : 'templates/gabriel/gabrielController.html'
     });
 
-    /*Lucas routes*/
+    /*
+    * Lucas Rutes, when call 'website+thisRoute' take user to toDoListTemplate.html
+    */
     $routeProvider.when('/lucas', {
         controller : TodoCtrl,
         templateUrl : 'templates/lucas/toDoListTemplate.html'
